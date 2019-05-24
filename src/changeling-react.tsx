08c3c2a8addf0e26056ca58dd842f5422f4e125e
Karl von Randow <karl@xk72.com>
2019-05-24 13:41:44 +1200

    Input element

--- src/changeling-react.tsx
+++ src/changeling-react.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Changeable, Changeling } from './changeling'
+import { Changeable, Changeling, forFuncs } from './changeling'
 
 export function wrapComponent<R, P extends Changeable<R>>(Component: React.ComponentType<Changeable<R> & P>) {
 	return <T, K extends Match<T, R>>(props: Subtract<P, Changeable<R>> & { changeling: Changeling<T>, name: K }) => {
@@ -16,3 +16,47 @@ type Match<T, R> = {
 }[keyof T]
 
 type Subtract<T extends T1, T1 extends object> = Pick<T, Exclude<keyof T, keyof T1>>
+
+type ElementAttributes = React.InputHTMLAttributes<HTMLInputElement>
+
+interface ChangelingInputProps<T, K extends keyof T> extends ElementAttributes {
+	changeling: Changeling<T>
+	changelingProperty: K
+}
+export class ChangelingInput<T, K extends keyof T> extends React.Component<ChangelingInputProps<T, K>> {
+
+	public render() {
+		const { changeling, changelingProperty, ...rest } = this.props
+		const value = changeling.prop(changelingProperty).value
+		return (
+			<input value={`${value}` || ''} onChange={this.onChange} {...rest} />
+		)
+	}
+
+	private onChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+		this.props.changeling.prop(this.props.changelingProperty).onChange(this.convertValue(evt.target.value))
+	}
+
+	private convertValue = (value: string): T[K] => {
+		return value as any
+	}
+}
+
+// interface TTT {
+// 	blah: string
+// }
+// let cc: TTT = {
+// 	blah: 'what',
+// }
+// const c = forFuncs(
+// 	() => cc, 
+// 	(newValue: TTT) => {
+// 		cc = newValue
+// 		return
+// 	}
+// )
+
+
+// (
+// 	<ChangelingInput changeling={} changelingProperty="" />
+// )
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
 export { Changeable, ChangeableState, Changeling, forComponentProps, forComponentState, forFuncs } from './changeling'
-export { wrapComponent } from './changeling-react'
+export { wrapComponent, ChangelingInput as Input } from './changeling-react'

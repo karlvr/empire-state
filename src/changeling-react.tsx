87c773a67239b688401fc3e7f60002aba8dcb4c9
Karl von Randow <karl@xk72.com>
2019-05-27 16:55:26 +1200

    Better Input components

--- src/changeling-react.tsx
+++ src/changeling-react.tsx
@@ -1,41 +1,73 @@
 import * as React from 'react'
 import { Snapshot, Controller } from './changeling'
 import { Omit, Subtract, CompatibleKeys } from './utilities';
-import { KEY, PROPERTY, KEYABLE } from './types';
+import { KEY, KEYABLE, ANDTHIS, ANDTHISPROPERTY } from './types';
 
 export function wrapComponent<R, P extends Snapshot<R>>(Component: React.ComponentType<Snapshot<R> & P>) {
-	return <T, K extends CompatibleKeys<KEYABLE<T>, R>>(props: Subtract<P, Snapshot<R>> & { controller: Controller<T>, prop: K }) => {
+	return <T, K extends CompatibleKeys<KEYABLE<ANDTHIS<T>>, R>>(props: Subtract<P, Snapshot<R>> & { controller: Controller<T>, prop: K }) => {
 		const { controller, prop, ...rest } = props
-		const c = controller.snapshot(prop as any as KEY<T>)
+		const c = prop !== 'this' ? (controller as any as Controller<T>).snapshot(prop as KEY<T>) : controller.snapshot()
 		return (
 			<Component value={c.value} onChange={c.onChange} {...rest as any} />
 		)
 	}
 }
 
-interface InputProps<T, K extends KEY<T>> extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'> {
+interface WrapComponentConvertType<T, K extends KEY<T> | 'this', R> {
 	controller: Controller<T>
-	convert?: (value: string) => PROPERTY<T, K>
 	prop: K
+	convert: (value: R) => ANDTHISPROPERTY<T, K>
+	display?: (value: ANDTHISPROPERTY<T, K> | undefined) => R
 }
 
-export class Input<T, K extends KEY<T>> extends React.Component<InputProps<T, K>> {
+function wrapComponentConvert<R, P extends Snapshot<R>>(Component: React.ComponentType<Snapshot<R> & P>) {
+	return <T, K extends KEY<ANDTHIS<T>>>(props: Subtract<P, Snapshot<R>> & WrapComponentConvertType<T, K, R>) => {
+		const { controller, prop, convert, display, ...rest } = props
+		const c = prop !== 'this' ? (controller as any as Controller<T>).snapshot(prop as KEY<T>) : controller.snapshot()
+		return (
+			<Component value={display ? display(c.value as any) : c.value} onChange={(value) => { c.onChange(convert(value) as any) }} {...rest as any} />
+		)
+	}
+}
+
+/**
+ * Convert a component that accepts values of type R to a component that accepts values of type S.
+ * @param Component A component that accepts a Snapshot<R>
+ * @param convert A function to convert from R to S
+ * @param display A function to convert from S to R
+ */
+export function convertComponent<R, S, P extends Snapshot<R>>(Component: React.ComponentType<Snapshot<R> & P>, 
+		convert: (value: R) => S, 
+		display?: (value: S | undefined) => R
+	) {
+	return (props: Subtract<P, Snapshot<R>> & Snapshot<S>) => {
+		const { value, onChange, ...rest } = props
+		return (
+			<Component value={display ? display(value as any) : value} onChange={(value) => { onChange(convert(value) as any) }} {...rest as any} />
+		)
+	}
+}
+
+interface BaseInputProps<T> extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'>, Snapshot<T> {
+	convert?: (value: string) => T
+}
+
+class BaseInput<T> extends React.Component<BaseInputProps<T>> {
 
 	public render() {
-		const { controller, prop, convert, ...rest } = this.props
-		const value = controller.snapshot(prop).value
+		const { value, onChange, convert, ...rest } = this.props
 		return (
 			<input value={value !== undefined && value !== null ? `${value}` : ''} onChange={this.onChange} {...rest} />
 		)
 	}
 
 	private onChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
-		this.props.controller.snapshot(this.props.prop).onChange(this.convertValue(evt.target.value))
+		this.props.onChange(this.convertValue(evt.target.value))
 	}
 
-	private convertValue = (value: string): PROPERTY<T, K> => {
+	private convertValue = (value: string): T => {
 		if (this.props.convert) {
-			return this.props.convert(value)
+			return this.props.convert(value) as T
 		} else {
 			return value as any
 		}
@@ -43,19 +75,57 @@ export class Input<T, K extends KEY<T>> extends React.Component<InputProps<T, K>
 
 }
 
-interface LazyInputProps<T, K extends KEY<T>> extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'defaultValue' | 'onBlur'> {
-	controller: Controller<T>
-	convert?: (value: string) => PROPERTY<T, K> | undefined
-	prop: K
-	display?: (value: PROPERTY<T, K>) => string
-}
-
-export class LazyInput<T, K extends KEY<T>> extends React.Component<LazyInputProps<T, K>> {
+class StringInput extends React.Component<Omit<BaseInputProps<string>, 'convert'>> {
 
 	public render() {
-		const { controller, prop, convert, display, ...rest } = this.props
+		const { ...rest } = this.props
+		return (
+			<BaseInput {...rest} />
+		)
+	}
 
-		const value = controller.snapshot(prop).value
+}
+
+const NumberInput = convertComponent(
+	StringInput, 
+	(value) => parseInt(value), 
+	(value) => value !== undefined ? `${value}` : '',
+)
+
+interface CheckableInputProps<T> extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'checked' | 'onChange' | 'value'>, Snapshot<T> {
+	checkedValue: T
+	uncheckedValue?: T
+}
+
+class CheckableInput<T> extends React.Component<CheckableInputProps<T>> {
+
+	public render() {
+		const { value, checkedValue, uncheckedValue, onChange, ...rest } = this.props
+		return (
+			<input checked={value === checkedValue as any} onChange={this.onChange} value={checkedValue !== undefined && checkedValue !== null ? `${checkedValue}` : ''} {...rest} />
+		)
+	}
+
+	private onChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+		if (evt.target.checked) {
+			this.props.onChange(this.props.checkedValue)
+		} else {
+			this.props.onChange(this.props.uncheckedValue as any)
+		}
+	}
+
+}
+
+interface LazyBaseInputProps<T> extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange' | 'defaultValue' | 'onBlur' | 'convert' | 'display'>, Snapshot<T> {
+	convert?: (value: string) => T | undefined
+	display?: (value: T | undefined) => string
+}
+
+class LazyBaseInput<T> extends React.Component<LazyBaseInputProps<T>> {
+
+	public render() {
+		const { value, onChange, ...rest } = this.props
+
 		const displayValue = this.displayValue(value)
 		return (
 			<input key={displayValue} defaultValue={displayValue} onBlur={this.onBlur} {...rest} />
@@ -63,21 +133,20 @@ export class LazyInput<T, K extends KEY<T>> extends React.Component<LazyInputPro
 	}
 
 	private onBlur = (evt: React.FocusEvent<HTMLInputElement>) => {
-		const { controller, prop } = this.props
+		const { onChange } = this.props
 
-		const snapshot = controller.snapshot(prop)
 		const value = this.convertValue(evt.target.value)
 		if (value !== undefined) {
-			snapshot.onChange(value)
+			onChange(value)
 		} else if (evt.target.value === '') {
-			snapshot.onChange(undefined as any as PROPERTY<T, K>)
+			onChange(undefined as any as T)
 		} else {
-			evt.target.value = this.displayValue(snapshot.value)
+			evt.target.value = this.displayValue(this.props.value)
 			evt.target.select()
 		}
 	}
 
-	private displayValue = (value: PROPERTY<T, K>): string => {
+	private displayValue = (value: T): string => {
 		if (this.props.display) {
 			return this.props.display(value)
 		}
@@ -89,7 +158,7 @@ export class LazyInput<T, K extends KEY<T>> extends React.Component<LazyInputPro
 		}
 	}
 
-	private convertValue = (value: string): PROPERTY<T, K> | undefined => {
+	private convertValue = (value: string): T | undefined => {
 		if (this.props.convert) {
 			return this.props.convert(value)
 		} else {
@@ -99,53 +168,41 @@ export class LazyInput<T, K extends KEY<T>> extends React.Component<LazyInputPro
 
 }
 
-interface CheckableInputProps<T, K extends KEY<T>> extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'checked' | 'onChange' | 'value'> {
-	controller: Controller<T>
-	prop: K
-	value?: KEYABLE<T>[K]
-}
-
-export class CheckableInput<T, K extends KEY<T>> extends React.Component<CheckableInputProps<T, K>> {
+class LazyStringInput extends React.Component<Omit<LazyBaseInputProps<string>, 'convert' | 'display'>> {
 
 	public render() {
-		const { controller, prop, value, ...rest } = this.props
-		const selectedValue = controller.snapshot(prop).value
+		const { ...rest } = this.props
 		return (
-			<input checked={value === selectedValue as any} onChange={this.onChange} value={value !== undefined && value !== null ? `${value}` : ''} {...rest} />
+			<LazyBaseInput {...rest} />
 		)
 	}
 
-	private onChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
-		if (evt.target.checked) {
-			this.props.controller.snapshot(this.props.prop).onChange(this.props.value as KEYABLE<T>[K])
-		} else {
-			this.props.controller.snapshot(this.props.prop).onChange(undefined as any)
-		}
-	}
-
 }
 
-interface TextAreaProps<T, K extends KEY<T>> extends Omit<React.InputHTMLAttributes<HTMLTextAreaElement>, 'value' | 'onChange'> {
-	controller: Controller<T>
-	convert?: (value: string) => PROPERTY<T, K>
-	prop: K
+const LazyNumberInput = convertComponent(
+	LazyStringInput, 
+	(value) => parseInt(value), 
+	(value) => value !== undefined ? `${value}` : '',
+)
+
+interface BaseTextAreaProps<T> extends Omit<React.InputHTMLAttributes<HTMLTextAreaElement>, 'value' | 'onChange'>, Snapshot<T> {
+	convert?: (value: string) => T
 }
 
-export class TextArea<T, K extends KEY<T>> extends React.Component<TextAreaProps<T, K>> {
+class BaseTextArea<T> extends React.Component<BaseTextAreaProps<T>> {
 
 	public render() {
-		const { controller, prop, convert, ...rest } = this.props
-		const value = controller.snapshot(prop).value
+		const { value, onChange, convert, ...rest } = this.props
 		return (
 			<textarea value={value !== undefined && value !== null ? `${value}` : ''} onChange={this.onChange} {...rest} />
 		)
 	}
 
 	private onChange = (evt: React.ChangeEvent<HTMLTextAreaElement>) => {
-		this.props.controller.snapshot(this.props.prop).onChange(this.convertValue(evt.target.value))
+		this.props.onChange(this.convertValue(evt.target.value))
 	}
 
-	private convertValue = (value: string): PROPERTY<T, K> => {
+	private convertValue = (value: string): T => {
 		if (this.props.convert) {
 			return this.props.convert(value)
 		} else {
@@ -153,4 +210,29 @@ export class TextArea<T, K extends KEY<T>> extends React.Component<TextAreaProps
 		}
 	}
 
+}
+
+class StringTextArea extends React.Component<Omit<BaseTextAreaProps<string>, 'convert'>> {
+
+	public render() {
+		const { ...rest } = this.props
+		return (
+			<BaseTextArea {...rest} />
+		)
+	}
+
+}
+
+export const Input = {
+	Checkable: wrapComponent(CheckableInput),
+	Generic: wrapComponentConvert(StringInput),
+	String: wrapComponent(StringInput),
+	Number: wrapComponent(NumberInput),
+
+	LazyGeneric: wrapComponentConvert(LazyStringInput),
+	LazyString: wrapComponent(LazyStringInput),
+	LazyNumber: wrapComponent(LazyNumberInput),
+
+	TextArea: wrapComponent(StringTextArea),
+	TextAreaGeneric: wrapComponentConvert(StringTextArea),
 }
--- src/types.ts
+++ src/types.ts
@@ -5,3 +5,11 @@
 export type KEYABLE<T> = T extends object ? T : never
 export type KEY<T> = keyof KEYABLE<T>
 export type PROPERTY<T, K extends keyof KEYABLE<T>> = KEYABLE<T>[K]
+
+/** Add a property named 'this' that is of the type of the given T */
+export type ANDTHIS<T> = T & { this: T }
+
+/** Like PROPERTY<T, K> but includes support for the magic `this` property that refers to the type T itself */
+export type ANDTHISPROPERTY<T, K extends KEY<T> | 'this'> = 
+	K extends 'this' ? T 
+	: PROPERTY<T, Exclude<K, 'this'>>

fc55c37484dbb9469d42eeeaf3f256fd4e2fb2bd
Karl von Randow <karl@xk72.com>
2020-12-02 19:49:03 +1300

    Improvements for React hooks
    Controllers are now memoized so they don’t trigger re-renders unless the value changes. They also always use the latest value, and don’t have to wait for a re-render to use the latest value. This last part is a significant change in behaviour, but one that should make using the controller much more consistent with hooks… if you use the same controller twice before a re-render you’ll not clobber the previous update anymore.

--- packages/formalities/src/index.ts
+++ packages/formalities/src/index.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useMemo, useRef, useState } from 'react'
 import { withFuncs, Controller, Snapshot } from 'immutable-state-controller'
 export * from './components'
 export { Controller, Snapshot } from 'immutable-state-controller'
@@ -6,18 +6,44 @@ export { Controller, Snapshot } from 'immutable-state-controller'
 export function useController<T>(initialState: T): Controller<T>
 export function useController<T>(value: T, setValue: (newValue: T) => void): Controller<T>
 export function useController<T>(value: T, setValue?: (newValue: T) => void): Controller<T> {
-	let controller: Controller<T>
 	if (setValue === undefined) {
 		// eslint-disable-next-line react-hooks/rules-of-hooks
 		const [state, setState] = useState(value)
-		controller = withFuncs(() => state, setState)
+		// eslint-disable-next-line react-hooks/rules-of-hooks
+		return useSnapshotController({ value: state, setValue: setState })
 	} else {
-		controller = withFuncs(() => value, setValue)
+		// eslint-disable-next-line react-hooks/rules-of-hooks
+		return useSnapshotController({ value, setValue })
 	}
-
-	return controller
 }
 
 export function useSnapshotController<T>(snapshot: Snapshot<T>): Controller<T> {
-	return withFuncs(() => snapshot.value, snapshot.setValue)
+	const currentSnapshotValue = useRef(snapshot.value)
+	const currentSnapshotSetValue = useRef(snapshot.setValue)
+	currentSnapshotValue.current = snapshot.value
+	currentSnapshotSetValue.current = snapshot.setValue
+
+	/* We use useMemo so that the controller doesn't change unless the value changes,
+	   so it doesn't trigger a re-render when we use it in deps in a component.
+	 */
+	const mainController = useMemo(
+		() => {
+			return withFuncs(
+				() => currentSnapshotValue.current, 
+				(newValue) => {
+					currentSnapshotValue.current = newValue
+					currentSnapshotSetValue.current(newValue)
+				},
+			)
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
+			const ignore = snapshot.value
+		},
+		[snapshot.value],
+	)
+
+	/* Because we reuse the same Controller instance if the snapshot value is the same,
+	   we must remove all of the change listeners as the render pass will add them again.
+	 */
+	mainController.removeAllChangeListeners()
+	return mainController
 }
